bd22684da83b3c1d187159ccce943ae704b8eca1
shaurCode <shaurya.rishi@yahoo.com>
2024-02-28 20:14:12 +0400

    Made sketchybar media song length longer

--- config/sketchybar/items/media.sh
+++ config/sketchybar/items/media.sh
@@ -3,7 +3,7 @@ media=(
   icon.background.image=media.artwork
   icon.background.image.corner_radius=9
   script="$PLUGIN_DIR/media.sh"
-  label.max_chars=15
+  label.max_chars=30
   scroll_texts=on
   updates=on
 )
